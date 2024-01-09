18cb74e782a2dfc19836455d5747a55fa5b8b716
ramirezfernando <ramirez.fernando2003@gmail.com>
2024-01-09 16:59:40 -0600

    feat: add footer section

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Header from "./components/Header"
 import About from './components/About';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           <Route path="/projects" element={<Projects />} />
           <Route path="/contact" element={<Contact />} />
         </Routes>
+        <Footer />
       </>
     </BrowserRouter>
   )
---
+++ src/components/Footer.tsx
@@ -0,0 +1,59 @@
+import { FaLinkedin, FaGithub } from 'react-icons/fa';
+import { IoMdMail } from 'react-icons/io';
+import Hidden from '@mui/material/Hidden';
+import Tooltip from '@mui/material/Tooltip';
+
+import '../Styles/Footer.css';
+
+const Footer = () => {
+    return (
+      <div className='footer-container'>
+        <Hidden xsDown>
+          <div className='footer-copyright'>
+            <p>Made with ❤️ by Fernando</p>
+          </div>
+        </Hidden>
+        <div className='footer-links-container'>
+          
+          <div className='footer-link github'>
+          <Tooltip title='GitHub' placement='top' arrow>
+            <a
+              href='https://github.com/ramirezfernando'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <FaGithub style={{ animationDelay: '100ms' }} />
+            </a>
+          </Tooltip>
+        </div>
+
+        <div className='footer-link email'>
+          <Tooltip title='Email' placement='top' arrow>
+            <a
+              href='mailto:ramirez.fernando2003@gmail.com'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <IoMdMail style={{ animationDelay: '200ms' }} size='1.7rem' />
+            </a>
+          </Tooltip>
+        </div>
+
+        <div className='footer-link linkedin'>
+          <Tooltip title='LinkedIn' placement='top' arrow>
+            <a
+              href='https://linkedin.com/in/fndo/'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <FaLinkedin style={{ animationDelay: '300ms' }} size='1.7rem' />
+            </a>
+          </Tooltip>
+        </div>
+
+        </div>
+      </div>
+    );
+  };
+  
+  export default Footer;
