861f5741d492c3ac00416eb242c385334d09a448
ramirezfernando <ramirez.fernando2003@gmail.com>
2024-01-09 01:27:26 -0600

    feat: add contacts section

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from "./components/Header"
 import About from './components/About';
 import Projects from './components/Projects';
+import Contact from './components/Contact';
 
 function App() {
   return (
@@ -12,7 +13,7 @@ function App() {
         <Routes>
           <Route path="/about" element={<About />} />
           <Route path="/projects" element={<Projects />} />
-          <Route path="/contact" element={<>Contact</>} />
+          <Route path="/contact" element={<Contact />} />
         </Routes>
       </>
     </BrowserRouter>
---
+++ src/components/Contact.tsx
@@ -0,0 +1,48 @@
+import Fade from "@mui/material/Fade";
+import '../Styles/Contact.css';
+import { BLUE, YELLOW, PINK } from '../constants';
+
+const Contact = () => {
+  return (
+    <div className={`contact-container`}>
+      <Fade in timeout={{ enter: 1200 }}>
+        <div className='contact-content'>
+          <h2>
+            Got an <span style={{ color: PINK }}>interesting</span>{' '}
+            project you're working on, or just want to say{' '}
+            <span style={{ color: PINK }}>hi</span>?
+          </h2>
+          <p>
+            I'd love to hear from you!
+            <br />
+            <br />
+            The best way to reach me is to shoot me an email at{' '}
+            <a
+              href='mailto:ramirez.fernando2003@gmail.com'
+              style={{ color: YELLOW }}
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+                ramirez.fernando2003@gmail.com
+            </a>
+            .
+            <br />
+            <br />
+            You can also connect with me on{' '}
+            <a
+              href='https://www.linkedin.com/in/fndo/'
+              style={{ color: BLUE }}
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              LinkedIn
+            </a>
+            .
+          </p>
+        </div>
+      </Fade>
+    </div>
+  );
+};
+
+export default Contact;
