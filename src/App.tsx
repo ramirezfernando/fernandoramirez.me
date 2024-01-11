2d852b1857fe3bdcdce8956d84896bec57d08b84
ramirezfernando <ramirez.fernando2003@gmail.com>
2024-01-11 17:19:26 -0600

    fix: vercel deployment

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ function App() {
       <>
         <Header />
         <Routes>
-          <Route path='/' element={<Navigate to="/about" />} />
+          <Route path='' element={<Navigate to="/about" />} />
           <Route path="/about" element={<About />} />
           <Route path="/projects" element={<Projects />} />
           <Route path="/contact" element={<Contact />} />
